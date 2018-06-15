06850ac2d6ec5825144d2084f5f52ed513861a19
Yashwardhan Pauranik <yash@appointy.com>
2018-06-15 14:39:56 +0530

    moment js added for time stamps

--- public/js/index.js
+++ public/js/index.js
@@ -9,16 +9,18 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(msg) {
+    var formattedTime = moment(msg.createdAT).format('h:mm a');
     var li = $('<li></li>');
-    li.text(`${msg.from}: ${msg.text}`);
+    li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(msg) {
+    var formattedTime = moment(msg.createdAT).format('h:mm a');
     var li = $('<li></li>');
     var a = $('<a target="_blank">Live location</a>');
-    li.text(`${msg.from}: `);
+    li.text(`${msg.from} ${formattedTime}: `);
     a.attr('href', msg.url);
     li.append(a);
     $('#messages').append(li);
